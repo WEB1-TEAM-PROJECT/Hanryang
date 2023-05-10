d98db35f26108805e16648f4e9818fc760951156
Park JeongHo <popramel25@gmail.com>
2023-05-10 17:31:23 +0900

    homebotton js 실험

---
+++ js/tohome.js
@@ -0,0 +1,14 @@
+function twist() {
+    console.log("TWIST!")
+}
+function shout() {
+    console.log("SHOUT!")
+}
+
+const tasButton = document.querySelector('#tas');
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+tasButton.addEventListener('click', twist)
+tasButton.addEventListener('click', shout)
