656d7fe74cfa33a040ecbc1a19f6fda9e39ba746
Park JeongHo <popramel25@gmail.com>
2023-05-11 17:13:55 +0900

    링크 연습

--- js/tohome.js
+++ js/tohome.js
@@ -12,3 +12,16 @@ const tasButton = document.querySelector('#tas');
 
 tasButton.addEventListener('click', twist)
 tasButton.addEventListener('click', shout)
+
+//홈페이지보내기
+document.querySelector('#home').addEventListener('click',e=>{
+    window.location.href = '/list.html';
+});
+
+//홈페이지보내기 통합
+function linkTo() {
+    window.location.href = '/list.html';
+}
+
+let toMap = document.querySelectorAll('.link');
+toMap[0].addEventListener('click', linkTo)
