2b2341e53c4795457c066de6074f13e344ad6495
Park JeongHo <popramel25@gmail.com>
2023-05-15 00:40:21 +0900

    map_page hover js 구현

--- JavaScript/map_page.js
+++ JavaScript/map_page.js
@@ -5,9 +5,17 @@ function mouseOut() {
     popup_button.removeAttribute('class', 'hover')
 }
 
-
 let popbtns = Array.from(document.querySelectorAll('button'));
 for(let popbtn of popbtns){
     popbtn.addEventListener('mouseover', mouseOn)
     popbtn.addEventListener('mouseout', mouseOut)
 }
+
+const el = document.querySelector('.container');
+const hiddenDiv = document.querySelector('.dog');
+el.addEventListener('mouseover', function handleMouseOver() {
+    hiddenDiv.style.display = 'block';
+ });
+el.addEventListener('mouseout', function handleMouseOut() {
+    hiddenDiv.style.display = 'none';
+});
