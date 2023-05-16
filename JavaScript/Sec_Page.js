f1f531da5e7e975ddabde1dc5e5f20562b232d8b
Park JeongHo <popramel25@gmail.com>
2023-05-16 10:55:03 +0900

    listpage 각 지역 구성

--- JavaScript/Sec_Page.js
+++ JavaScript/Sec_Page.js
@@ -1,26 +1,26 @@
 //성수페이지로 이동
 document.querySelector('#sungsu').addEventListener('click',e=>{
-    window.location.href = '../HTML/sungsu_page.html';
+    window.location.href = '../HTML/ListPage_sungsu.html';
 });
 
 //홍대페이지로 이동
 document.querySelector('#hongdae').addEventListener('click',e=>{
-    window.location.href = '../HTML/hongdae_page.html';
+    window.location.href = '../HTML/ListPage_hongdae.html';
 });
 
 //혜화페이지로 이동
 document.querySelector('#hyehwa').addEventListener('click',e=>{
-    window.location.href = '../HTML/map_page.html';
+    window.location.href = '../HTML/ListPage_hyehwa.html';
 });
 
 //북촌페이지로 이동
 document.querySelector('#bukchon').addEventListener('click',e=>{
-    window.location.href = '../HTML/map_page.html';
+    window.location.href = '../HTML/ListPage_bukchon.html';
 });
 
 //강남페이지로 이동
 document.querySelector('#gangnam').addEventListener('click',e=>{
-    window.location.href = '../HTML/map_page.html';
+    window.location.href = '../HTML/ListPage_gangnam.html';
 });
 
 //추천페이지로 이동
