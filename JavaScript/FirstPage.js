5419281c5fdf722799f828b5a4b68d7aa0bdad37
DESKTOP-03N3F3K\myfam <doheopark@hanyang.ac.kr>
2023-05-22 01:37:51 +0900

    js와 html 분리

--- JavaScript/FirstPage.js
+++ JavaScript/FirstPage.js
@@ -1,3 +1,7 @@
 function changeImage() {
     document.getElementById("hn").src = "../IMG/FirstPage/girl.png";
 }
+
+function changeImage1() {
+    document.getElementById("hn").src = "../IMG/FirstPage/boy.png";
+}
