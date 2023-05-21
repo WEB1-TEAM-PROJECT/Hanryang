e2f911985fb516194645f751623d5d4c09855812
Park JeongHo <popramel25@gmail.com>
2023-05-21 12:19:44 +0900

    TeamPage 이미지 위치오류 및 코드 정리

--- JavaScript/TeamPage.js
+++ JavaScript/TeamPage.js
@@ -1,8 +1,9 @@
-//parkjeongho img change
+// imgs = [pjh, ken, yjh, pdh]
 let frontimg = Array.from(document.querySelectorAll('.front'));
 let behindimg = Array.from(document.querySelectorAll('.behind'));
 
-//마우스 올릴시 띄우기
+//마우스 올릴시 뒤 이미지 display
+//각 이미지마다 mouseover 이벤트 적용
 for(let i=0; i<frontimg.length; i++){
   frontimg[i].addEventListener('mouseover', e=>{
       behindimg[i].style.display = 'inline';
@@ -10,70 +11,10 @@ for(let i=0; i<frontimg.length; i++){
   });
 }
 
-//마우스 내릴시
+//마우스 내릴시 원래대로 
 for(let i=0; i<frontimg.length; i++){
   frontimg[i].addEventListener('mouseout', e=>{
       behindimg[i].style.display = 'none';
       frontimg[i].style.display = 'inline';
-  });
-}
-
-//kenlee img change
-let frontimg1 = Array.from(document.querySelectorAll('.front1'));
-let behindimg1 = Array.from(document.querySelectorAll('.behind1'));
-
-//마우스 올릴시 띄우기
-for(let i=0; i<frontimg1.length; i++){
-  frontimg1[i].addEventListener('mouseover', e=>{
-      behindimg1[i].style.display = 'inline';
-      frontimg1[i].style.display = 'none';
-  });
-}
-
-//마우스 내릴시
-for(let i=0; i<frontimg1.length; i++){
-  frontimg1[i].addEventListener('mouseout', e=>{
-      behindimg1[i].style.display = 'none';
-      frontimg1[i].style.display = 'inline';
-  });
-}
-
-//yujihye img change
-let frontimg2 = Array.from(document.querySelectorAll('.front2'));
-let behindimg2 = Array.from(document.querySelectorAll('.behind2'));
-
-//마우스 올릴시 띄우기
-for(let i=0; i<frontimg2.length; i++){
-  frontimg2[i].addEventListener('mouseover', e=>{
-      behindimg2[i].style.display = 'inline';
-      frontimg2[i].style.display = 'none';
-  });
-}
-
-//마우스 내릴시
-for(let i=0; i<frontimg2.length; i++){
-  frontimg2[i].addEventListener('mouseout', e=>{
-      behindimg2[i].style.display = 'none';
-      frontimg2[i].style.display = 'inline';
-  });
-}
-
-//parkdohye img change
-let frontimg3 = Array.from(document.querySelectorAll('.front3'));
-let behindimg3 = Array.from(document.querySelectorAll('.behind3'));
-
-//마우스 올릴시 띄우기
-for(let i=0; i<frontimg3.length; i++){
-  frontimg3[i].addEventListener('mouseover', e=>{
-      behindimg3[i].style.display = 'inline';
-      frontimg3[i].style.display = 'none';
-  });
-}
-
-//마우스 내릴시
-for(let i=0; i<frontimg3.length; i++){
-  frontimg3[i].addEventListener('mouseout', e=>{
-      behindimg3[i].style.display = 'none';
-      frontimg3[i].style.display = 'inline';
   });
 }
