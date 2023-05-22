2ddb9c5be80b8adebf8b1217ecf1f65dab882fc9
Park JeongHo <popramel25@gmail.com>
2023-05-22 10:41:57 +0900

    혜화 이미지 추가 및 클릭시 리스트로 이동

--- JavaScript/Map_hyehwa.js
+++ JavaScript/Map_hyehwa.js
@@ -18,36 +18,35 @@ function initMap() {
     var locations = [
         
         //카페테라피 http://cafetherapy.net 37.5803866,127.0026872
-        ['<div class="wrap"><div class="text-box"><h4>카페테라피</h4><div class="img-box"><img class="marker" src="../IMG/concert (1).png"></div><a target="_blank" href="http://cafetherapy.net/"><p>홈페이지 방문하기</p></a></div>', 37.58053751843618, 127.0053050105693],
+        ['<div class="wrap"><div class="text-box"><a href="#firstPic"><h4>카페테라피</h4><div class="img-box"><img class="marker" src="../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_cafetherapy.jpeg"></div><p>자세한 정보</p></a></div>', 37.58053751843618, 127.0053050105693],
         
-        //아이띵소 https://www.ithinksoshop.co.kr 37.5788284,127.0020601
-        ['<div class="wrap"><div class="text-box"><h4>아이쇼룸</h4><div class="img-box"><img class="marker" src="../IMG/concert (1).png"></div><a target="_blank" href="https://www.ithinksoshop.co.kr"><p>홈페이지 방문하기</p></a></div>', 37.57883616542004, 127.00466915630915],
+        //아이띵소 쇼룸 https://www.ithinksoshop.co.kr 37.5788284,127.0020601
+        ['<div class="wrap"><div class="text-box"><a href="#fifthPic"><h4>아이띵쏘</h4><div class="img-box"><img class="marker" src="../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_ithinkso.png"></div><p>자세한 정보</p></a></div>', 37.57883616542004, 127.00466915630915],
         
         //혜화그림제작소 http://grimfactory.kr/bbs/board.php?bo_table=store 37.58256860015284, 126.99853768173308
-        ['<div class="wrap"><div class="text-box"><h4>혜화 그림 제작소</h4><div class="img-box"><img class="marker" src="../IMG/concert (1).png"></div><a target="_blank" href="http://grimfactory.kr/bbs/board.php?bo_table=store"><p>홈페이지 방문하기</p></a></div>', 37.58256860015284, 126.99853768173308],
+        ['<div class="wrap"><div class="text-box"><a href="#fourthPic"><h4>그림 제작소</h4><div class="img-box"><img class="marker" src="../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_drawing_cafe.webp"></div><p>자세한 정보</p></a></div>', 37.58256860015284, 126.99853768173308],
         
         //낙산공원 37.58067910552169, 127.0075389282773
-        ['<div class="wrap"><div class="text-box"><h4>낙산공원</h4><div class="img-box"><img class="marker" src="../IMG/concert (1).png"></div><a target="_blank" href="http://grimfactory.kr/bbs/board.php?bo_table=store"><p>홈페이지 방문하기</p></a></div>', 37.58067910552169, 127.0075389282773],
+        ['<div class="wrap"><div class="text-box"><a href="#seventhPic"><h4>낙산공원</h4><div class="img-box"><img class="marker" src="../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_naksang.jpeg"></div><p>자세한 정보</p></a></div>', 37.58067910552169, 127.0075389282773],
         
         //상상의문 방탈출 37.584101682742634, 127.00087386333585
-        ['<div class="wrap"><div class="text-box"><h4>상상의문</h4><div class="img-box"><img class="marker" src="../IMG/concert (1).png"></div><a target="_blank" href="http://grimfactory.kr/bbs/board.php?bo_table=store"><p>홈페이지 방문하기</p></a></div>', 37.584101682742634, 127.00087386333585],
+        ['<div class="wrap"><div class="text-box"><a href="#ninePic"><h4>상상의문</h4><div class="img-box"><img class="marker" src="../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_sangsang_door.png"></div><p>자세한 정보</p></a></div>', 37.584101682742634, 127.00087386333585],
         
         //퍼퓸그라피 향수 쇼륨 37.582517648902375, 127.00291110791656
-        ['<div class="wrap"><div class="text-box"><h4>퍼퓸그라피</h4><div class="img-box"><img class="marker" src="../IMG/concert (1).png"></div><a target="_blank" href="http://grimfactory.kr/bbs/board.php?bo_table=store"><p>홈페이지 방문하기</p></a></div>', 37.582517648902375, 127.00291110791656],
+        ['<div class="wrap"><div class="text-box"><a href="#eightPic"><h4>퍼퓸그라피</h4><div class="img-box"><img class="marker" src="../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_perfume.png"></div><p>자세한 정보</p></a></div>', 37.582517648902375, 127.00291110791656],
         
         //마로니에공원 37.58051858622418, 127.0028579529348
-        ['<div class="wrap"><div class="text-box"><h4>마로니에공원</h4><div class="img-box"><img class="marker" src="../IMG/concert (1).png"></div><a target="_blank" href="http://grimfactory.kr/bbs/board.php?bo_table=store"><p>홈페이지 방문하기</p></a></div>', 37.58051858622418, 127.0028579529348],
+        ['<div class="wrap"><div class="text-box"><a href="#sixthPic"><h4>마로니에공원</h4><div class="img-box"><img class="marker" src="../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_maronie.jpeg"></div><p>자세한 정보</p></a></div>', 37.58051858622418, 127.0028579529348],
         
         //창경궁 37.57883191342931, 126.99499960598965
-        ['<a target="_blank" href="https://cdg.go.kr/">창경궁</a>', 37.57883191342931, 126.99499960598965]
-        
+        ['<div class="wrap"><div class="text-box"><a href="#secondPic"><h4>창경궁</h4><div class="img-box"><img class="marker" src="../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_changguonggung.jpeg"><p>자세한 정보</p></a></div>', 37.57883191342931, 126.99499960598965],
         
         ]
 
     
     
     //마커 이미지
-    var customicon = '../IMG/ListPage/Marker/Hongdae_2.png'
+    var customicon = '../IMG/ListPage/Marker/Haehwa_2.png'
 
     //인포윈도우
     var infowindow = new google.maps.InfoWindow();
