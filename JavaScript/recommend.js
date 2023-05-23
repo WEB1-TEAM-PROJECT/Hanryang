eb9fb6be732049a2299568b276c23fdd7992d93b
jihyeyu33 <jihyeyu33@gmail.com>
2023-05-24 01:31:49 +0900

    추천 함수 완성, rec_q3에 결과 뜨도록 함

--- JavaScript/recommend.js
+++ JavaScript/recommend.js
@@ -1,17 +1,72 @@
 let arr1 = new Array(); //고궁 및 박물관
-arr1[0]="<a href='https://www.nfm.go.kr/home/subIndex/1239.do#'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_park.jpeg' border='0'><br>국립민속박물관</a>"
-arr1[1]="<a href='https://cgg.cha.go.kr/agapp/main/index.do?siteCd=CGG'><img src='../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_changguonggung.jpeg' border='0'><br>창경궁</a>"
-arr1[2]="<a href='https://www.chf.or.kr/cont/view/fest/month/menu/210?thisPage=1&idx=109015&searchCategory1=600&searchCategory2=&searchField=all&searchDate=202304&weekSel=&searchText='><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_chang.jpeg' border='0'><br>창덕궁 달빛기행</a>"
+arr1[0]="<a href='https://www.nfm.go.kr/home/subIndex/1239.do#'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_park.jpeg' border='0' width=60%><br>국립민속박물관</a>"
+arr1[1]="<a href='https://cgg.cha.go.kr/agapp/main/index.do?siteCd=CGG'><img src='../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_changguonggung.jpeg' border='0'  width=60%><br>창경궁</a>"
+arr1[2]="<a href='https://www.chf.or.kr/cont/view/fest/month/menu/210?thisPage=1&idx=109015&searchCategory1=600&searchCategory2=&searchField=all&searchDate=202304&weekSel=&searchText='><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_chang.jpeg' border='0'  width=60%><br>창덕궁 달빛기행</a>"
 
 let arr2 = new Array(); //전시
-arr2[0]="<a href='https://groundseesaw.co.kr/product/%eb%82%98%ed%83%88%eb%a6%ac-%ec%b9%b4%eb%a5%b4%ed%91%b8%ec%85%b4%ec%bd%94-%ec%82%ac%ec%a7%84%ec%a0%84/'><img src='../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_natali.png' border='0' width='100%' height='100%'><br>나탈리 카르푸셴코 사진전, 전시/a>";
-arr2[1]="<a href='https://www.sangsangmadang.com/display/detail/2356'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_earth.jpeg' border='0' width='100%' height='100%'>지구에 커튼을 쳐 줄게, 전시</a>";
-arr2[2]="<a href='https://tickets.interpark.com/goods/23004816?app_tapbar_state=hide&'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_museum.jpeg' border='0' width='100%' height='100%'>왓츠 유어 컬러, 전시</a>";
-arr2[3]="<a href='https://www.galleryafternoon.com/'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_afternoon.png' border='0' width='100%' height='100%'>갤러리 애프터눈</a>";
-arr2[4]="<a href='https://www.mmca.go.kr/'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_museum.jpeg' border='0' width='100%' height='100%'>국립 현대 미술관</a>";
-arr2[5]="<a href='https://museumhanmi.or.kr/'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_hanmi.jpeg' border='0' width='100%' height='100%'>뮤지엄 한미 - 삼청 별관</a>";
+arr2[0]="<a href='https://groundseesaw.co.kr/product/%eb%82%98%ed%83%88%eb%a6%ac-%ec%b9%b4%eb%a5%b4%ed%91%b8%ec%85%b4%ec%bd%94-%ec%82%ac%ec%a7%84%ec%a0%84/'><img src='../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_natali.png' border='0' width=60%><br>나탈리 카르푸셴코 사진전, 전시/a>";
+arr2[1]="<a href='https://www.sangsangmadang.com/display/detail/2356'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_earth.jpeg' border='0' width=60%><br>지구에 커튼을 쳐 줄게, 전시</a>";
+arr2[2]="<a href='https://tickets.interpark.com/goods/23004816?app_tapbar_state=hide&'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_museum.jpeg' border='0' width=60%><br>왓츠 유어 컬러, 전시</a>";
+arr2[3]="<a href='https://www.galleryafternoon.com/'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_afternoon.png' border='0' width=60%><br>갤러리 애프터눈</a>";
+arr2[4]="<a href='https://www.mmca.go.kr/'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_museum.jpeg' border='0' width=60%><br>국립 현대 미술관</a>";
+arr2[5]="<a href='https://museumhanmi.or.kr/'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_hanmi.jpeg' border='0' width=60%><br>뮤지엄 한미 - 삼청 별관</a>";
+
+let arr3 = new Array(); //공방
+arr3[0]="<a href='https://www.instagram.com/pottring/'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_dojagi.png' border='0' width=60%><br>도자기 포트링 공방</a>";
+arr3[1]="<a href='https://121lemaldupays.com/'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_121.png' border='0' width=60%><br>121르말뒤페이, 향수 공방</a>";
+arr3[2]="<a href='ringuniv.com/campus/kn/'><img src='../IMG/List_Thumbnail/Gangnam_thumbnail/Gangnam_banji.jpeg' border='0' width=60%><br>강남 반지대학, 반지 공방</a>";
+
+let arr4 = new Array(); //공연 관람
+arr4[0]="<a href='http://ticket.interpark.com/contents/Ranking/RankList?pKind=01009&pType=D'><img src='../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_dehakro.jpeg' border='0' width=60%><br>대학로 연극로, 연극</a>";
+arr4[1]="<a href='http://indiespace.tistory.com/'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_indie.jpeg' border='0' width=60%><br>인디스페이스, 독립 영화 관람</a>";
+
+let arr5 = new Array(); //공원
+arr5[0]="<a href='https://seoulforest.or.kr/info/park-info'><img src='../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_seoulpark.jpeg' border='0' width=60%><br>서울숲 공원</a>";
+arr5[1]="<a href='https://parks.seoul.go.kr/template/sub/naksan.do'><img src='../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_naksang.jpeg' border='0' width=60%><br>낙산 공원</a>";
+arr5[2]="<a href='http://parks.seoul.go.kr/parks/detailView.do?pIdx=72&tabTarget=1'><img src='../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_maronie.jpeg' border='0' width=60%><br>마로니에 공원</a>";
+arr5[3]="<a href='http://parks.seoul.go.kr/parks/detailView.do?pIdx=109'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_park.jpeg' border='0' width=60%><br>삼청 공원</a>";
+
+let arr6 = new Array(); //복합문화공간
+arr6[0]="<a href='http://seongsu.realworld.to/'><img src='../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_realworld.png' border='0' width=60%><br>리얼 월드, 성수</a>";
+arr6[1]="<a href='https://www.instagram.com/gonggan_wadiz/'><img src='../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_konggang.jpeg' border='0' width=60%><br>공간 와디즈</a>";
+arr6[2]="<a href='https://www.tfactory.co.kr/main/store'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_tfactory.webp' border='0' width=60%><br>T factory</a>";
+arr6[3]="<a href='https://상상의문.com/layout/res/home.php?go=rev.make'><img src='../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_sangsang_door.png' border='0' width=60%><br>상상의 문</a>";
+arr6[4]="<a href='https://www.lguplus.com/benefit/dailyteum'><img src='../IMG/List_Thumbnail/Gangnam_thumbnail/Gangnam_ilsang.jpeg' border='0' width=60%><br>일상 비일상의 틈</a>";
+arr6[5]="<a href='https://hanok.seoul.go.kr/front/kor/town/town01.do'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_hanok.jpeg' border='0' width=60%><br>북촌 한옥 마을</a>";
+
+let arr7 = new Array(); //실내 액티비티
+arr7[0]="<a href='https://www.finalnine.co.kr/?module=Board&action=SiteBoard&sMode=VIEW_FORM&iBrdNo=10&iBrdContNo=2&sBrdContRe=0&sSearchField=&sSearchValue=&CurrentPage=1'><img src='../IMG/List_Thumbnail/Gangnam_thumbnail/Gangnam_finalNine.jpeg' border='0' width=60%><br>파이널나인, 홀덤펍</a>";
+arr7[1]="<a href='http://vr-zone.co.kr/board/bbs/board.php?bo_table=franchise_sch&wr_id=3&page=3'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_vr.jpeg' border='0' width=60%><br>홍대 VR아케이드</a>";
+arr7[2]="<a href='https://여기유.com/place/25446'><img src='../IMG/List_Thumbnail/Gangnam_thumbnail/Gangnam_templebowling.png' border='0' width=60%><br>템플스트라이크, 락볼링장</a>";
+arr7[3]="<a href='http://www.tmon.co.kr/deal/2932452370'><img src='../IMG/List_Thumbnail/Gangnam_thumbnail/Gangnam_campVR.png' border='0' width=60%><br>캠프 VR</a>";
+arr7[4]="<a href='https://www.signalhunter.co.kr/branch/?v=38dd815e66db'><img src='../IMG/List_Thumbnail/Gangnam_thumbnail/Gangnam_signalhunter.jpeg' border='0' width=60%><br>시그널 헌터, 방탈출 카페</a>";
+arr7[5]="<a href='https://www.instagram.com/coin_tennis_pub/'><img src='../IMG/List_Thumbnail/Gangnam_thumbnail/Gangnam_tennis.jpeg' border='0' width=60%><br>코인테니스펍</a>";
+
+let arr8 = new Array(); //카페, 힐링
+arr8[0]="<a href='https://www.instagram.com/greenlab_seoulforest/'><img src='../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_greenrap.png' border='0' width=60%><br>그린랩, 힐링 스튜디오</a>";
+arr8[1]="<a href='https://seongsumuseum.com/'><img src='../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_art.png' border='0' width=60%><br>성수 미술관, 드로잉 카페</a>";
+arr8[2]="<a href='https://dgdg.modoo.at/'><img src='../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_dingul.webp' border='0' width=60%><br>딩굴딩굴 알타미라, 만화 카페</a>";
+arr8[3]="<a href='http://cafetherapy.net'><img src='../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_cafetherapy.jpeg' border='0' width=60%><br>카페테라피, 상담 카페</a>";
+arr8[4]="<a href='http://grimfactory.kr/bbs/board.php?bo_table=store'><img src='../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_drawing_cafe.webp' border='0' width=60%><br>혜화 그림 제작소, 드로잉 카페</a>";
+arr8[5]="<a href='https://m.starfield.co.kr/coexmall/tenant/starfieldLibrary'><img src='../IMG/List_Thumbnail/Gangnam_thumbnail/Gangnam_library.jpeg' border='0' width=60%><br>별마당 도서관</a>";
+arr8[6]="<a href='https://www.instagram.com/acorn_caricature/?hl=ko'><img src='../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_todori.webp' border='0' width=60%><br>도토리 캐리커쳐</a>";
+arr8[7]="<a href='http://gffactory.co.kr/main/main.php'><img src='../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_halaboji.jpeg' border='0' width=60%><br>할아버지 공장, 카페</a>";
+arr8[8]="<a href='https://www.instagram.com/psb_coffeebar/'><img src='../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_coffee.jpeg' border='0' width=60%><br>피에스비 커비바, 카페</a>";
+
+let arr9 = new Array(); //팝업스토어
+arr9[0]="<a href='https://www.markgonzaleskorea.com/product/single.html?product_no=444&cate_no=58&display_group=1'><img src=''../IMG/List_Thumbnail/Sungsu_thumbnail/Sungsu_mark.png' border='0' width=60%><br>마크곤잘레스, 팝업스토어</a>";
+arr9[1]="<a href='https://www.musinsaterrace.com/'><img src=''../IMG/List_Thumbnail/Hongdae_thumbnail/Hongdae_musinsa.jpeg' border='0' width=60%><br>무신사 테라스</a>";
+arr9[2]="<a href='https://www.ithinksoshop.co.kr'><img src=''../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_ithinkso.png' border='0' width=60%><br>아이띵소, 쇼룸</a>";
+arr9[3]="<a href='https://perfumegraphy.com/product/list_project.html'><img src=''../IMG/List_Thumbnail/Hyehwa_thumbnail/Hyehwa_perfume.png' border='0' width=60%><br>퍼퓸그라피, 쇼룸</a>";
+arr9[4]="<a href='https://doasis.kr/16/?&bmode=view&idx=341149&t=board'><img src=''../IMG/List_Thumbnail/Gangnam_thumbnail/Gangnam_ chanel.jpeg' border='0' width=60%><br>샤넬 팝업스토어</a>";
+arr9[5]="<a href='https://naver.me/xEqtxCp8'><img src=''../IMG/List_Thumbnail/Bukchon_thumbnail/Bukchon_store.jpeg' border='0' width=60%><br>탬버린즈 플래그쉽스토어</a>";
 
 let style = new Array(); //선택한 스타일
+
+function transDiv(){
+    document.getElementById('q3').style.display="none";
+    document.getElementById('result').style.display="inline-block";
+}
 
 document.getElementById('submit').onclick = function() {
     if (document.getElementById('arr1').checked) {
@@ -20,10 +75,37 @@ document.getElementById('submit').onclick = function() {
     if (document.getElementById('arr2').checked) {
         style.push.apply(style,arr2);
     }
+    if (document.getElementById('arr3').checked) {
+        style.push.apply(style,arr3);
+    }
+    if (document.getElementById('arr4').checked) {
+        style.push.apply(style,arr4);
+    }
+    if (document.getElementById('arr5').checked) {
+        style.push.apply(style,arr5);
+    }
+    if (document.getElementById('arr6').checked) {
+        style.push.apply(style,arr6);
+    }
+    if (document.getElementById('arr7').checked) {
+        style.push.apply(style,arr7);
+    }
+    if (document.getElementById('arr8').checked) {
+        style.push.apply(style,arr8);
+    }
+    if (document.getElementById('arr9').checked) {
+        style.push.apply(style,arr1);
+    }
+
+    transDiv();
+
 }
+
+
 
 function recommend(){
     let rand = Math.floor(Math.random()*(style.length))
     
-    document.getElementById("result").innerHTML = style[rand];
+    document.getElementById("show").innerHTML = style[rand];
+    document.getElementById("after").style.display="inline-block";
 }
